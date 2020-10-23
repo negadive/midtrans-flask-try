1a82ec2461b7ea8f96a3f1a99340973e1415a407
TEGUH\Teguh <teguh@talentkita.com>
2020-10-23 14:45:04 +0700

    init commit

---
+++ web.py
@@ -0,0 +1,81 @@
+import datetime
+import json
+from flask import Flask, render_template, request, jsonify
+from secret import SERVER_KEY, CLIENT_KEY
+
+from midtransclient import Snap, CoreApi
+
+app = Flask(__name__)
+
+#==============#
+# Using SNAP
+#==============#
+
+# Very simple Snap checkout
+@app.route('/simple_checkout')
+def simple_checkout():
+    snap = Snap(
+        is_production=False,
+        server_key=SERVER_KEY,
+        client_key=CLIENT_KEY,
+    )
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    transaction = snap.create_transaction({
+        "transaction_details": {
+            "order_id": "order-id-python-"+timestamp,
+            "gross_amount": 210000
+        },
+        "user_id": 11,
+        "customer_details": {
+            "first_name": "TEST",
+            "last_name": "MIDTRANSER",
+            "email": "test@midtrans.com",
+            "phone": "+628123456",
+            "billing_address": {
+                "first_name": "TEST",
+                "last_name": "MIDTRANSER",
+                "email": "test@midtrans.com",
+                "phone": "081 2233 44-55",
+                "address": "Sudirman",
+                "city": "Jakarta",
+                "postal_code": "12190",
+                "country_code": "IDN"
+            },
+        },
+        "credit_card": {
+            "secure": True,
+            "channel": "migs",
+            "save_card": True,
+            "bank": "bca",
+            "installment": {
+                "required": False,
+                "terms": {
+                "bni": [3, 6, 12],
+                "mandiri": [3, 6, 12],
+                "cimb": [3],
+                "bca": [3, 6, 12],
+                "offline": [6, 12]
+                }
+            },
+            "whitelist_bins": [
+                    "48111111",
+                    "41111111"
+                ]
+        }
+    })
+    return render_template('simple_checkout.html',
+        token = transaction['token'],
+        client_key = snap.api_config.client_key,
+        transaction=transaction)
+
+#==============#
+# Run Flask app
+#==============#
+
+# Homepage of this web app
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5000,host='0.0.0.0')
